82059b31c87ff110cc714b15339deabaf2fead12
LOVELLOUD <lovelloud@gmail.com>
2025-05-30 16:28:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,6 @@ global.canal = 'https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
 global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
