d4d31ae529aa03ac11d3f51a8ab6cfb6fd311f63
LOVELLOUD <lovelloud@gmail.com>
2025-05-30 16:22:14 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,26 +2,31 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
+//*⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯*
 
 global.mods = []
 global.prems = []
 global.owner = [
-  ['51928303585', 'Samsito', true],
+  ['51928303585', 'Samsit', true],
 ]
 
+//*⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯*
 
-global.namebot = 'Anya Forger'
 global.author = 'Samsit'
+global.namebot = 'Anya Forger'
 
+//*⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯*
 
-global.namecanal = 'Prueba'
-global.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
+global.namecanal = 'hello'
 global.idcanal = '120363274577422945@newsletter'
+global.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
 
+//*⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯*
 
 global.multiplier = 69 
 global.maxwarn = '2'
 
+//*⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯⭒⋯*
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
