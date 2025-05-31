cd6dc0604a39873624634b80dbff8a2854472a50
LOVELLOUD <lovelloud@gmail.com>
2025-05-31 14:51:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -197,7 +197,7 @@ async function connectionUpdate(update) {
   }
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
 if (reason == 405) {
-await fs.unlinkSync("./sessions/" + "creds.json")
+unlinkSync("./sessions/creds.json")
 console.log(chalk.bold.redBright(`Conexión replazada, Por favor espere un momento me voy a reiniciar...\nSi aparecen error vuelve a iniciar con : npm start`)) 
 process.send('reset')}
 if (connection === 'close') {
