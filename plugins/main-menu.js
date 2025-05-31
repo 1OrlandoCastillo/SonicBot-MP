633f193d2a88ec77c190b1cd23ede6555aab08f4
LOVELLOUD <lovelloud@gmail.com>
2025-05-31 16:30:36 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -16,26 +16,18 @@ let tags = {
   'owner': '• Creador',
 }
 
-const defaultMenu = {
-  before: `
+const menuMessage = `
  • Hola soy ${botname}
 
 *﹙ ✿ ﹚Principal*
 https://whatsapp.com/channel/0029VbAZUQ3002T9KZfx2O1M
 *﹙ ✿ ﹚Secundario*
 https://whatsapp.com/channel/0029Vb3oShrICVfiTWhDHM13
-%readmore
-\t\t\t
-`.trimStart(),
-  header: '┌─ 「  *`%category`*  」', 
-  body: '┃❏%cmd %islimit %isPremium\n',
-  footer: '╰─────────────',
-  after: ``,
-}
+`;
 
 let img = 'https://telegra.ph/file/72f984396bb1db415d153.jpg'
     
-   await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
+   await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m)
 
   } catch (e) {
     conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
@@ -44,4 +36,16 @@ let img = 'https://telegra.ph/file/72f984396bb1db415d153.jpg'
 }
 
 handler.command = ['menu', 'help', 'menú'] 
+
 export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+let horas = Math.floor(ms / 3600000)
+let minutos = Math.floor(ms / 60000) % 60
+let segundos = Math.floor(ms / 1000) % 60
+  console.log({ ms, horas, minutos, segundos })
+return [horas, minutos, segundos].map((v) => v.toString().padStart(2, 0)).join(":")
+}
