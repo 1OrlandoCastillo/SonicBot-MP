34751cac6b2495b1717526ad73db5cd348ccd153
LOVELLOUD <lovelloud@gmail.com>
2025-06-01 08:10:04 -0500

    Update serbot-code.js

--- plugins/serbot-code.js
+++ plugins/serbot-code.js
@@ -107,7 +107,7 @@ const { connection, lastDisconnect, isNewLogin, qr } = update
 if (isNewLogin) sock.isInit = false
 if (qr && !mcode) {
 if (m?.chat) {
-txtQR = await conn.sendMessage(m.chat, { image: await qrcode.toBuffer(qr, { scale: 8 }), caption: "rcanal" }, { quoted: m })
+txtQR = await conn.sendMessage(m.chat, { image: await qrcode.toBuffer(qr, { scale: 8 }), caption: rtx, m, null, rcanal)
 } else {
 return 
 }
