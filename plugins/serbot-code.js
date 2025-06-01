9d897107a4ebfcedc488725c7bbea97979d898cb
LOVELLOUD <lovelloud@gmail.com>
2025-06-01 12:49:20 -0500

    Update serbot-code.js

--- plugins/serbot-code.js
+++ plugins/serbot-code.js
@@ -195,9 +195,7 @@ sock.isInit = true
 global.conns.push(sock)
 await joinChannels(sock)
 
-await parent.reply(m.chat, args[0] ? `*✿ Te conectaste exitosamente, ahora eres un subbot*\n\n> *Sigue el canal para estar atento a todas las actualizaciones y avisos.*\n\nhttps://whatsapp.com/channel/0029VbAZUQ3002T9KZfx2O1M`, m, rcanal)
-    await sleep(5000)
-    if (args[0]) return
+await parent.reply(m.chat, args[0] ? `*✿ Te conectaste exitosamente, ahora eres un subbot*\n\n> *Sigue el canal para estar atento a todas las actualizaciones y avisos.*\n\nhttps://whatsapp.com/channel/0029VbAZUQ3002T9KZfx2O1M` : null, m, rcanal)
 
 }}
 setInterval(async () => {
