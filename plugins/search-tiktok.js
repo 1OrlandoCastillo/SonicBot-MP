aa1ea1e6d0c467422098b6151a116a32d97b13c0
LOVELLOUD <lovelloud@gmail.com>
2025-06-01 19:58:01 -0500

    Update search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -1,35 +1,38 @@
-.import axios from 'axios'
-import Starlights from '@LOVELLOUD/Scraper'
+const axios = require('axios');
+const LOVELLOUD = require('@LOVELLOUD/Scraper');
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* SOKIC Edit`, m, rcanal)
+  if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* hi Edit`, m, rcanal);
   
-  await m.react('🕓')
-  let img = `./storage/img/menu.jpg`
+  await m.react('🕓');
+  let img = `./storage/img/menu.jpg`;
+
   try {
-    let data = await LOVELLOUD.tiktokSearch(text)
+    let data = await LOVELLOUD.tiktokSearch(text);
 
     if (data && data.length > 0) {
-      let txt = `*乂  T I K T O K  -  S E A R C H*`
-      for (let i = 0; i < (50 <= data.length ? 50 : data.length); i++) {
-        let video = data[i]
-        txt += `\n\n`
-        txt += `  *» Nro* : ${i + 1}\n`
-        txt += `  *» Título* : ${video.title}\n`
-        txt += `  *» Autor* : ${video.author}\n`
-        txt += `  *» Url* : ${video.url}`
+      let txt = `*乂  T I K T O K  -  S E A R C H*`;
+      for (let i = 0; i < (data.length <= 50 ? data.length : 50); i++) {
+        let video = data[i];
+        txt += `\n\n`;
+        txt += `  *» Nro* : ${i + 1}\n`;
+        txt += `  *» Título* : ${video.title}\n`;
+        txt += `  *» Autor* : ${video.author}\n`;
+        txt += `  *» Url* : ${video.url}`;
       }
-      await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
-      await m.react('✅')
+      await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal);
+      await m.react('✅');
     } else {
-      await conn.react('✖️')
+      await m.react('✖️');
     }
-  } catch {
-    await m.react('✖️')
+  } catch (e) {
+    console.error(e);
+    await m.react('✖️');
   }
-}
-handler.tags = ['search']
-handler.help = ['tiktoksearch *<búsqueda>*']
-handler.command = ['tiktoksearch', 'tiktoks']
+};
 
-export default handler
+handler.tags = ['search'];
+handler.help = ['tiktoksearch *<búsqueda>*'];
+handler.command = ['tiktoksearch', 'tiktoks'];
+
+module.exports = handler;
