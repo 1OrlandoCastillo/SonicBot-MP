0132dfc163e0faeee179208d4ca695611a9dc391
LOVELLOUD <lovelloud@gmail.com>
2025-06-01 19:50:28 -0500

    Update search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -1,22 +1,17 @@
-import axios from 'axios'
+.import axios from 'axios'
 import Starlights from '@LOVELLOUD/Scraper'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) {
-    return conn.reply(
-      m.chat,
-      `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* Sonic Edit`, m, rcanal)
-
+  if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* SOKIC Edit`, m, rcanal)
+  
   await m.react('🕓')
   let img = `./storage/img/menu.jpg`
-
   try {
     let data = await LOVELLOUD.tiktokSearch(text)
 
     if (data && data.length > 0) {
       let txt = `*乂  T I K T O K  -  S E A R C H*`
-
-      for (let i = 0; i < (data.length >= 50 ? 50 : data.length); i++) {
+      for (let i = 0; i < (50 <= data.length ? 50 : data.length); i++) {
         let video = data[i]
         txt += `\n\n`
         txt += `  *» Nro* : ${i + 1}\n`
@@ -24,17 +19,15 @@ let handler = async (m, { conn, usedPrefix, command, text, args }) => {
         txt += `  *» Autor* : ${video.author}\n`
         txt += `  *» Url* : ${video.url}`
       }
-
       await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
       await m.react('✅')
     } else {
-      await m.react('✖️')
+      await conn.react('✖️')
     }
   } catch {
     await m.react('✖️')
   }
 }
-
 handler.tags = ['search']
 handler.help = ['tiktoksearch *<búsqueda>*']
 handler.command = ['tiktoksearch', 'tiktoks']
