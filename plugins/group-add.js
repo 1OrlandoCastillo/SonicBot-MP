946b56366456d9fb553dbe4a53b35f624a50aa00
LOVELLOUD <lovelloud@gmail.com>
2025-06-03 11:13:02 -0500

    Update group-add.js

--- plugins/group-add.js
+++ plugins/group-add.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, args, usedPrefix, command, isROwner }) => {
 
-let addte = `✿ Ingresa el número del usuario que deseas añadir.`
+let addte = `Ingresa el número del usuario que deseas añadir.`
 
 if (!args[0]) return m.reply(addte, m.chat)
 
 let user = args[0].replace(/[^0-9]/g, '') + '@s.whatsapp.net'
 await conn.groupParticipantsUpdate(m.chat, [user], 'add')
-m.reply(`✿ Usuario añadido al grupo.`)
-m.reply(`✿ ¡Has sido añadido al grupo!`, user)
+m.reply(`Usuario añadido al grupo.`)
+m.reply(`¡Has sido añadido al grupo!`, user)
 
 }
 
