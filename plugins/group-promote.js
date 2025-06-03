040e3befcb9e3bbd99c70c194a7fb660a8cc7c88
LOVELLOUD <lovelloud@gmail.com>
2025-06-03 11:14:10 -0500

    Update group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, participants }) => {
 
-let promote = `✿ Menciona al usuario que deseas promover a administrador.`
+let promote = `Menciona al usuario que deseas promover a administrador.`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(promote, m.chat, { mentions: conn.parseMention(promote) })
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 await conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-m.reply(`✿ Usuario promovido a administrador.`)
+m.reply(`Usuario promovido a administrador.`)
 }
 
 handler.help = ['promote *<@user>*']
