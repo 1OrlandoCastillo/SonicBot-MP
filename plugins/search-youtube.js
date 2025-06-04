d4f60e1ebd4698598340ed010b05c3a2a8418c23
LOVELLOUD <lovelloud@gmail.com>
2025-06-03 21:37:51 -0500

    Update search-youtube.js

--- plugins/search-youtube.js
+++ plugins/search-youtube.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, `✿ Ingresa el nombre del video que deseas buscar en *YouTube.*`, m, rcanal)
+  if (!text) return conn.reply(m.chat, `𖧊 Hola, necesito que me proporciones el nombre del video *Youtube* que deseas Buscar.`, m, rcanal)
 
   await m.react('🕓')
   let img = `./storage/img/menu.jpg`
