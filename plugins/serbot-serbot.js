dfebb21f3225c42162d9311b1be14e0c74a9268e
LOVELLOUD <lovelloud@gmail.com>
2025-06-08 13:02:43 -0500

    Update serbot-serbot.js

--- plugins/serbot-serbot.js
+++ plugins/serbot-serbot.js
@@ -23,8 +23,7 @@ import { makeWASocket } from '../lib/simple.js';
 if (!(global.conns instanceof Array)) global.conns = [];
 
 let handler = async (m, { conn: star, args, usedPrefix, command, isOwner }) => {
-  let parent = await global.conn;
-
+  let parent = args[0] && args[0] == 'plz' ? _conn : await global.conn;
   if (!((args[0] && args[0] == 'plz') || (await global.conn).user.jid == parent.user.jid)) {
     return m.reply(`Este comando solo puede ser usado en el bot principal! wa.me/${global.conn.user.jid.split`@`[0]}?text=${usedPrefix}code`);
   }
