4fa044a1e75697189a213eff911d96a487232cdb
LOVELLOUD <lovelloud@gmail.com>
2025-06-07 22:25:03 -0500

    Update serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -13,14 +13,15 @@ let handler = async (m, { conn }) => {
     }
   })
 
-  let totalUsers = uniqueUsers.size
   let txt = `「 *• Searchs* 」\n\n`
 
     txt = `${totalUsers || 0}`
+  let totalUsers = uniqueUsers.size
   let count = 1
   for (let [jid, conn] of uniqueUsers.entries()) {
     let number = jid.split('@')[0]
     let name = conn.user?.name || 'Sin nombre'
+    txt = `${totalUsers || 0}`
     txt += `*◦Nro →* ${count}\n*◦Nombre →* ${name}\n*◦Fono →* wa.me/${number}\n\n`
     count++
   }
