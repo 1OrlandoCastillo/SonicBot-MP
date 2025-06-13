a658efdb121c336cde03eca59df06d4b8758166e
LOVELLOUD <lovelloud@gmail.com>
2025-06-13 13:11:35 -0500

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, participants }) => {
 let kickMsg = `🚩 Menciona al usuario que deseas eliminar.`
 
 if (!m.mentionedJid[0] && !m.quoted) 
-    return m.reply(kickMsg, m, rcanal { mentions: conn.parseMention(kickMsg) })
+    return m.reply(m.chat, kickMsg, m, rcanal, { mentions: conn.parseMention(kickMsg) })
 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
