765a9246bc1d4da53b280a247fe46556a237cd2f
LOVELLOUD <lovelloud@gmail.com>
2025-06-13 12:28:02 -0500

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -4,20 +4,20 @@ let handler = async (m, rcanal) => {
     let kickMsg = `🚩 Menciona al usuario que deseas eliminar.`
 
     if (!m.mentionedJid?.[0] && !m.quoted)
-        return m.reply(kickMsg, m.chat, { mentions: conn.parseMention(kickMsg) })
+        return m.reply(kickMsg, m.chat, { mentions: conn.parseMention(kickMsg) }, m, rcanal)
 
     let user = m.mentionedJid?.[0] ? m.mentionedJid[0] : m.quoted.sender
 
     // Verifica si el usuario es parte del grupo
     let isParticipant = participants.some(p => p.id === user)
-    if (!isParticipant) return m.reply('❌ El usuario no está en el grupo.')
+    if (!isParticipant) return m.reply('❌ El usuario no está en el grupo.', m.chat, {}, m, rcanal)
 
     // Intenta eliminar al usuario
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
 
     // Confirmaciones
-    m.reply(`🚩 Usuario eliminado del grupo.`)
-    m.reply(`Has sido eliminado del grupo.`, user)
+    m.reply(`🚩 Usuario eliminado del grupo.`, m.chat, {}, m, rcanal)
+    m.reply(`Has sido eliminado del grupo.`, user, {}, m, rcanal)
 }
 
 handler.help = ['kick']
