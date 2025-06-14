b215adcedb68b904846ab3d8d7cc291aa1fe3da4
LOVELLOUD <lovelloud@gmail.com>
2025-06-14 17:36:26 -0500

    Update set-packname.js

--- plugins/set-packname.js
+++ plugins/set-packname.js
@@ -1,13 +1,15 @@
 let handler = async (m, { args, usedPrefix, command }) => {
-  if (!args[0]) {
-    return conn.reply(m.chat,`𖧏 Hola, necesito que me proporciones el nombre del *Paquete* que deseas Poner.`, m, rcanal)
-  }
-  
+  if (!global.db.data.subbots?.[m.sender]) 
+    return m.reply('🚫 Este comando es solo para sub-bots conectados.')
+
+  if (!args[0]) 
+    return m.reply(`✦ Escribe el nombre del paquete.\nEj: ${usedPrefix + command} Mis Stickers`)
+
   global.db.data.users[m.sender].packname = args.join(' ')
-  return conn.reply(m.chat,`𖧏 Hola, el nombre del packname que proporcionastes se cambio a *${args.join(' ')}* Correctamente.`, m, rcanal)
+  m.reply(`✅ Packname actualizado a:\n*${args.join(' ')}*`)
 }
 
-handler.help = ['setpackname']
+handler.help = ['setpackname <texto>']
 handler.tags = ['set']
 handler.command = ['setpackname']
 
