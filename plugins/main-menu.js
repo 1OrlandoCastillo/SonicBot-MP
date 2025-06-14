72fb121c636216338c1d73f01c47a6874ced3104
LOVELLOUD <lovelloud@gmail.com>
2025-06-14 18:28:13 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -5,8 +5,8 @@ import { xpRange } from '../lib/levelling.js'
 
 const handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
-    const user = global.db.data.users[m.sender] || {}
-    const namebot = user.namebot || global.namebot
+    // Cargar nombre del bot personalizado por subbot
+const namebot = global.db.data.settings[conn.user.jid]?.namebot || global.namebot;
 
     const tags = {
       serbot: '• Subs - Bots',
