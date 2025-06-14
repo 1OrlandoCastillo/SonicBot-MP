4b11589581da1c409436c1af337b13dd4360edf1
LOVELLOUD <lovelloud@gmail.com>
2025-06-14 15:53:49 -0500

    Update set-packname.js

--- plugins/set-packname.js
+++ plugins/set-packname.js
@@ -1,10 +1,10 @@
 let handler = async (m, { args, usedPrefix, command }) => {
   if (!args[0]) {
-    return m.reply(`𖧏 Hola, debes ingresar el nombre del *Paquete* que deseas Poner.`, m, rcanal)
+    return conn.reply(m.chat,`𖧏 Hola, debes ingresar el nombre del *Paquete* que deseas Poner.`)
   }
   
   global.db.data.users[m.sender].packname = args.join(' ')
-  m.reply(`𖧏 Hola, el *Packname* se actualizado a *${args.join(' ')}* Correctamente.`, m, rcanal)
+  return conn.reply(m.chat,`𖧏 Hola, el *Packname* se actualizado a *${args.join(' ')}* Correctamente.`)
 }
 
 handler.help = ['setpackname']
