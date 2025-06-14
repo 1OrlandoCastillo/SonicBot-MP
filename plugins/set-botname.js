4a0eb06cc635ed2fc7e09a560f59b87d729b1a63
LOVELLOUD <lovelloud@gmail.com>
2025-06-14 18:32:10 -0500

    Update set-botname.js

--- plugins/set-botname.js
+++ plugins/set-botname.js
@@ -1,8 +1,10 @@
-.let handler = async (m, { args, conn }) => {
+let handler = async (m, { args, conn }) => {
   if (!args[0]) return m.reply('✳️ Ingresa el nuevo nombre del bot.\n\nEjemplo:\n.setbotname Bot');
   global.db.data.settings[conn.user.jid] = global.db.data.settings[conn.user.jid] || {};
   global.db.data.settings[conn.user.jid].namebot = args.join(" ");
   m.reply(`✅ El nombre del bot ha sido cambiado a:\n*${args.join(" ")}*`);
 };
-handler.command = ['setbotname'];
+handler.help = ['setname']
+handler.tags = ['set']
+handler.command = ['setname']
 export default handler;
