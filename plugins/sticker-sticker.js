ab33b0207457877d7612677b3e3a84a6539db383
LOVELLOUD <lovelloud@gmail.com>
2025-06-14 15:05:20 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -51,7 +51,6 @@ let handler = async (m, { conn, args }) => {
 handler.help = ['sticker']
 handler.tags = ['sticker']
 handler.command = ['s', 'sticker', 'stiker']
-handler.register = true
 
 export default handler
 
