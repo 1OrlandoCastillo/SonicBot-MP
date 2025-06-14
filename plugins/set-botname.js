e83a1fa3d1c03d17553bf05f02d50ee37b34cc68
LOVELLOUD <lovelloud@gmail.com>
2025-06-14 18:55:07 -0500

    Update set-botname.js

--- plugins/set-botname.js
+++ plugins/set-botname.js
@@ -1,13 +1,14 @@
-let handler = async (m, { args, text }) => {
-  if (!text) return m.reply(`✳️ Ingresa el nombre que deseas ponerle al bot. 📌 Ejemplo:
-.setbotname Bot`);
+let handler = async (m, { args, usedPrefix, command }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat,`𖧏 Hola, necesito que me proporciones el nombre del *Bot* que deseas Poner.`, m, rcanal)
+  }
+  
+  global.db.data.users[m.sender].namebot = args.join(' ')
+  return conn.reply(m.chat,`𖧏 Hola, el *Nombre* que proporcionastes se cambio a *${args.join(' ')}* Correctamente.`, m, rcanal)
+}
 
-  global.db.data.users[m.sender].namebot = text.trim();
-  m.reply(`✅ Has personalizado el nombre del bot. 📛 Nuevo nombre: *${text.trim()}*`);
-};
+handler.help = ['setbotname']
+handler.tags = ['set']
+handler.command = ['setbotname']
 
-handler.help = ['setbotname <nombre>'];
-handler.tags = ['tools'];
-handler.command = ['setbotname'];
-
-export default handler;
+export default handler
