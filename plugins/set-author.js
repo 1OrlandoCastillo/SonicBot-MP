15573d4677814a9835850092d300fef802222b4c
LOVELLOUD <lovelloud@gmail.com>
2025-06-14 16:30:01 -0500

    Update set-author.js

--- plugins/set-author.js
+++ plugins/set-author.js
@@ -1,10 +1,10 @@
 let handler = async (m, { args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat,`𖧏 Hola, necesito que me proporciones el nombre que del *Author* que deseas Poner.`, m, rcanal)
+    return conn.reply(m.chat,`𖧏 Hola, necesito que me proporciones el nombre del *Author* que deseas Poner.`, m, rcanal)
   }
   
   global.db.data.users[m.sender].author = args.join(' ')
-  return conn.reply(m.chat,`𖧏 Hola, el *Author* que proporcionastes se cambio a *${args.join(' ')}* Correctamente.`, m, rcanal)
+  return conn.reply(m.chat,`𖧏 Hola, el nombre del author que proporcionastes se cambio a *${args.join(' ')}* Correctamente.`, m, rcanal)
 }
 
 handler.help = ['setauthor']
