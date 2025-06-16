bca322eca0efde7ce140c96e499c09c17c1e7799
LOVELLOUD <lovelloud@gmail.com>
2025-06-16 14:48:33 -0500

    Update tools-deep.js

--- plugins/tools-deep.js
+++ plugins/tools-deep.js
@@ -1,20 +1,25 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `*Ingresa tu petición.*\n\n🪼 *Ejemplo:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
+  if (!text) return conn.reply(m.chat, `🚩 Ingrese su petición\n📌 *Ejemplo de uso:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
 
   await m.react('💬')
 
   try {
     let api = await fetch(`https://api-pbt.onrender.com/api/ai/model/deepseek?texto=${encodeURIComponent(text)}&apikey=8jkh5icbf05`)
-    let json = await res.json()
-    if (json.result) {
-      await conn.reply(m.chat, json.result, m, rcanal)
+    let json = await api.json()
+
+    if (json?.data) {
+      await conn.reply(m.chat, json.data.trim(), m, rcanal)
+    } else {
+      await m.react('✖️')
     }
-  } catch {}
+  } catch {
+    await m.react('✖️')
+  }
 }
 
-handler.help = ['deepsek']
+handler.help = ['deepseek *<petición>*']
 handler.tags = ['tools']
 handler.command = /^(deep|deepseek|deeps)$/i
 
