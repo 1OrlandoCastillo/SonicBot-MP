9c505c922e7cddff534b4d6407b7f400c5569615
LOVELLOUD <lovelloud@gmail.com>
2025-06-16 14:28:54 -0500

    Add files via upload

---
+++ plugins/tools-deep.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) return conn.reply(m.chat, `*Ingresa tu petición.*\n\n🪼 *Ejemplo:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
+
+  await m.react('💬')
+
+  try {
+    let res = await fetch(`https://api-pbt.onrender.com/api/ai/model/deepseek?texto=${encodeURIComponent(text)}`)
+    let json = await res.json()
+    if (json.result) {
+      await conn.reply(m.chat, json.result, m, rcanal)
+    }
+  } catch {}
+}
+
+handler.help = ['deepsek']
+handler.tags = ['tools']
+handler.command = /^(deep|deepseek|deeps)$/i
+
+export default handler
