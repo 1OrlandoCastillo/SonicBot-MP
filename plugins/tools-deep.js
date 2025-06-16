c637475d41b444269531a5f172ab093fa1de732f
LOVELLOUD <lovelloud@gmail.com>
2025-06-16 14:37:04 -0500

    Update tools-deep.js

--- plugins/tools-deep.js
+++ plugins/tools-deep.js
@@ -1,20 +1,41 @@
+import LOVELLOUD from '@LOVELLOUD/Scraper'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `*Ingresa tu petición.*\n\n🪼 *Ejemplo:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
+  if (!text) return conn.reply(m.chat, `Ingrese su petición*\n*Ejemplo de uso:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
 
   await m.react('💬')
 
   try {
-    let res = await fetch(`https://api-pbt.onrender.com/api/ai/model/deepseek?texto=${encodeURIComponent(text)}&apikey=8jkh5icbf05`)
-    let json = await res.json()
-    if (json.result) {
-      await conn.reply(m.chat, json.result, m, rcanal)
+    let { msg } = await LOVELLOUD.openAi(text)
+    await conn.reply(m.chat, msg, m, rcanal)
+  } catch {
+    try {
+      let { result } = await LOVELLOUD.ChatGpt(text)
+      await conn.reply(m.chat, result, m, rcanal)
+    } catch {
+      try {
+        let { result } = await LOVELLOUD.ChatGptV2(text)
+        await conn.reply(m.chat, result, m, rcanal)
+      } catch {
+        try {
+          let api = await fetch(`https://api-pbt.onrender.com/api/ai/model/deepseek?texto=${encodeURIComponent(text)}&apikey=8jkh5icbf05`)
+          let json = await api.json()
+
+          if (json?.data) {
+            await conn.reply(m.chat, json.data.trim(), m, rcanal)
+          } else {
+            await m.react('✖️')
+          }
+        } catch {
+          await m.react('✖️')
+        }
+      }
     }
-  } catch {}
+  }
 }
 
-handler.help = ['deepsek']
+handler.help = ['deepsek *<petición>*']
 handler.tags = ['tools']
 handler.command = /^(deep|deepseek|deeps)$/i
 
