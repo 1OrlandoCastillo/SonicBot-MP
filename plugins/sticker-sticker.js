2f95552ec4af1156e90b0dae3a25e6061a9476f7
LOVELLOUD <lovelloud@gmail.com>
2025-06-26 16:39:15 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, args }) => {
       buffer = await res.buffer()
 
     } else {
-      return conn.reply(m.chat, '𖧏 Hola, debes responder a una *Imagen, Sticker, Video, Webm o Tgs* para Convertirlo.', m, rcanal)
+      return conn.reply(m.chat, 'Debes responder a una imagen, sticker, video, webm o tgs para que funcione correctamente.', m, rcanal)
     }
 
     await m.react('🕓')
