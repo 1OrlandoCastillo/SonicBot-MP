c64ff90336501ea5e687245fad74863438f491b7
LOVELLOUD <lovelloud@gmail.com>
2025-06-27 11:01:45 -0500

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,42 +1,24 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args || !args[0]) {
-    return conn.reply(m.chat, `🚩 Ingresa un enlace del vídeo de *TikTok* junto al comando.\n\n` +
-      `> *Ejemplo:* ${usedPrefix + command} https://vm.tiktok.com/ZMrFCX5jf/`, m)
-  }
-
-  if (!args[0].match(/tiktok/i)) {
-    await m.react('✖️')
-    return conn.reply(m.chat, '🚫 Verifica que el enlace sea de TikTok.', m)
-  }
-
+if (!args || !args[0]) return conn.reply(m.chat, 'Para poder procesar tu solicitud, necesitas copiar y pegar el link directo del video de TikTok que deseas descargar.', m, rcanal)
   await m.react('🕓')
-
-  try {
-    const res = await fetch(`https://g-mini-ia.vercel.app/api/tiktok?url=${encodeURIComponent(args[0])}`)
-    const json = await res.json()
-
-    let {
-      title = 'Sin título',
-      author = 'Desconocido',
-      video: { url: dl_url } = {}
-    } = json?.data || {}
-
-    if (!dl_url) return await m.react('✖️')
-
-    let txt = `	✩  *Título:* ${title}\n`
-    txt += `	✩  *Autor:* ${author}`
-
-    await conn.sendFile(m.chat, dl_url, 'tiktok.mp4', txt, m, null, rcanal)
-    await m.react('✅')
-  } catch {
-    await m.react('✖️')
-  }
-}
-
-handler.help = ['tiktok *<url>*']
+try {
+const res = await fetch(`https://g-mini-ia.vercel.app/api/tiktok?url=${encodeURIComponent(args[0])}`)
+if (!res.ok) return
+const json = await res.json()
+if (!json || !json.video_url) return
+const { video_url, title, author } = json
+let txt = `\n`
+   txt += `Título: ${title}\n`
+   txt += `Autor: ${author}`
+await conn.sendFile(m.chat, video_url, 'tiktok.mp4', txt, m)
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['tiktok']
 handler.tags = ['downloader']
-handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
+handler.command = /^(tiktok|tt|tiktokdl|ttdl)$/i
 
 export default handler
