d9d93d91af542a3153d38a3e4da95bfcfab393f8
LOVELLOUD <lovelloud@gmail.com>
2025-07-01 10:15:29 -0500

    Update tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -3,8 +3,10 @@ let handler = async (m, { conn, command, args }) => {
 if (!args[0]) return conn.reply(m.chat,'Este comando requiere que proporciones una URL válida para poder continuar.', m, rcanal)
 await m.react('🕓')
 try {
-let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
-conn.sendFile(m.chat, ss, 'error.png', m, null, rcanal)
+    const res = await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)
+    const arrayBuffer = await res.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+    await conn.sendFile(m.chat, buffer, 'screenshot.png', '', m, null, { ...rcanal })
 await m.react('✅')
 } catch {
 await m.react('✖️')
