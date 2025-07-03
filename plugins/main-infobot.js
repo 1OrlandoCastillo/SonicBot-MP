120e0247d01eebd60281ffa2b4bbff7d66179505
LOVELLOUD <lovelloud@gmail.com>
2025-07-03 13:39:44 -0500

    Update main-infobot.js

--- plugins/main-infobot.js
+++ plugins/main-infobot.js
@@ -7,95 +7,98 @@ import { join } from 'path'
 import fs from 'fs'
 
 let format = sizeFormatter({
-    std: 'JEDEC',
-    decimalPlaces: 2,
-    keepTrailingZeroes: false,
-    render: (literal, symbol) => `${literal} ${symbol}B`,
+  std: 'JEDEC',
+  decimalPlaces: 2,
+  keepTrailingZeroes: false,
+  render: (literal, symbol) => `${literal} ${symbol}B`,
 })
 
 let handler = async (m, { conn, usedPrefix }) => {
-   let uniqueUsers = new Map()
+  let uniqueUsers = new Map()
 
-   global.conns.forEach((conn) => {
-     if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
-       uniqueUsers.set(conn.user.jid, conn)
-     }
-   })
-   let users = [...uniqueUsers.values()]
-   let totalUsers = users.length
-   let totalreg = Object.keys(global.db.data.users).length
-   let totalbots = Object.keys(global.db.data.settings).length
-   let totalStats = Object.values(global.db.data.stats).reduce((total, stat) => total + stat.total, 0)
-   const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
-   let totalchats = Object.keys(global.db.data.chats).length
-   let totalf = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length
-   const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
-   const used = process.memoryUsage()
-   const cpus = _cpus().map(cpu => {
-      cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
-      return cpu
-   })
-   const cpu = cpus.reduce((last, cpu, _, { length }) => {
-      last.total += cpu.total
-      last.speed += cpu.speed / length
-      last.times.user += cpu.times.user
-      last.times.nice += cpu.times.nice
-      last.times.sys += cpu.times.sys
-      last.times.idle += cpu.times.idle
-      last.times.irq += cpu.times.irq
-      return last
-   }, {
-      speed: 0,
-      total: 0,
-      times: {
-         user: 0,
-         nice: 0,
-         sys: 0,
-         idle: 0,
-         irq: 0
-      }
-   })
-   	let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
+  global.conns.forEach((conn) => {
+    if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+      uniqueUsers.set(conn.user.jid, conn)
     }
-    let muptime = clockString(_muptime)
-   let timestamp = speed()
-   let latensi = speed() - timestamp
-   let txt = `🪷 : Nombre largo :: ${nombreBot}`
-   txt += `🎀 : Nombre corto :: ${nombreBot}`
-   txt += `Prefijo :: ${usedPrefix}`
-   txt += `🍥 : Plugins :: ${totalf}`
-   txt += `🌸 : Speed :: ${latensi.toFixed(4)}`
-   txt += `💮 : Host :: Akirax`
-   txt += `🌼 : Conectad@ :: Akirax_1`
-   txt += `🍓 : Dueño :: Oculto por seguridad`
-   txt += `\n\n`
-   txt += `> LOVELLOUD Official`
+  })
 
-const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
-  const configPath = path.join('./JadiBots', botActual, 'config.json')
+  let users = [...uniqueUsers.values()]
+  let totalUsers = users.length
+  let totalreg = Object.keys(global.db.data.users).length
+  let totalbots = Object.keys(global.db.data.settings).length
+  let totalStats = Object.values(global.db.data.stats).reduce((total, stat) => total + stat.total, 0)
+  const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+  let totalchats = Object.keys(global.db.data.chats).length
+  let totalf = Object.values(global.plugins).filter(v => v.help && v.tags).length
+  const groupsIn = chats.filter(([id]) => id.endsWith('@g.us'))
+  const used = process.memoryUsage()
 
-let nombreBot = global.namebot || 'Anya Forger'
-let imgBot = './storage/img/menu3.jpg'
+  const cpus = _cpus().map(cpu => {
+    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+    return cpu
+  })
 
-if (fs.existsSync(configPath)) {
-try {
-const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
-if (config.name) nombreBot = config.name
-if (config.img) imgBot = config.img
-} catch (err) {
+  const cpu = cpus.reduce((last, cpu, _, { length }) => {
+    last.total += cpu.total
+    last.speed += cpu.speed / length
+    last.times.user += cpu.times.user
+    last.times.nice += cpu.times.nice
+    last.times.sys += cpu.times.sys
+    last.times.idle += cpu.times.idle
+    last.times.irq += cpu.times.irq
+    return last
+  }, {
+    speed: 0,
+    total: 0,
+    times: {
+      user: 0,
+      nice: 0,
+      sys: 0,
+      idle: 0,
+      irq: 0
+    }
+  })
+
+  let _muptime
+  if (process.send) {
+    process.send('uptime')
+    _muptime = await new Promise(resolve => {
+      process.once('message', resolve)
+      setTimeout(resolve, 1000)
+    }) * 1000
+  }
+  let muptime = clockString(_muptime)
+
+  const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
+  const configPath = join('./JadiBots', botActual, 'config.json')
+
+  let nombreBot = global.namebot || 'Anya Forger'
+  let imgBot = './storage/img/menu3.jpg'
+
+  if (fs.existsSync(configPath)) {
+    try {
+      const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
+      if (config.name) nombreBot = config.name
+      if (config.img) imgBot = config.img
+    } catch (err) { }
+  }
+
+  let timestamp = speed()
+  let latensi = speed() - timestamp
+
+  let txt = `🪷 : Nombre largo :: ${nombreBot}\n`
+  txt += `🎀 : Nombre corto :: ${nombreBot}\n`
+  txt += `⛩️ : Prefijo :: ${usedPrefix}\n`
+  txt += `🍥 : Plugins :: ${totalf}\n`
+  txt += `🌸 : Speed :: ${latensi.toFixed(4)} ms\n`
+  txt += `💮 : Host :: Akirax\n`
+  txt += `🌼 : Conectad@ :: Akirax_1\n`
+  txt += `🍓 : Dueño :: Oculto por seguridad\n`
+  txt += `\n> LOVELLOUD Official`
+
+  await conn.sendFile(m.chat, imgBot, 'thumbnail.jpg', txt, m, null, rcanal)
 }
-}
 
-await conn.sendFile(m.chat, imgBot, 'thumbnail.jpg', txt, m, null, rcanal)
-}
 handler.help = ['info']
 handler.tags = ['main']
 handler.command = ['info', 'infobot']
@@ -103,7 +106,7 @@ handler.command = ['info', 'infobot']
 export default handler
 
 function formatNumber(number) {
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
 function clockString(ms) {
