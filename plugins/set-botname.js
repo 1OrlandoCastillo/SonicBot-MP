d1117f0da27b7d933fff1f6d9b39068cae7643b8
LOVELLOUD <lovelloud@gmail.com>
2025-07-05 19:23:21 -0500

    Update set-botname.js

--- plugins/set-botname.js
+++ plugins/set-botname.js
@@ -1,11 +1,11 @@
-import fs from 'fs'
+³import fs from 'fs'
 import path from 'path'
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) return conn.reply(m.chat, `Necesitas ingresar un nombre para continuar.`, m, rcanal)
 
   const senderNumber = m.sender.replace(/[^0-9]/g, '')
-  const botPath = path.join('./JadiBots', senderNumber)
+  const botPath = path.join('./Serbot', senderNumber)
   const configPath = path.join(botPath, 'config.json')
 
   if (!fs.existsSync(botPath)) {
