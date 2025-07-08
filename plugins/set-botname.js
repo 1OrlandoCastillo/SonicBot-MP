dc54b3da85af97eb456c9b5eaec7677147bab7fc
LOVELLOUD <lovelloud@gmail.com>
2025-07-08 13:09:21 -0500

    Update set-botname.js

--- plugins/set-botname.js
+++ plugins/set-botname.js
@@ -1,15 +1,27 @@
 import fs from 'fs'
 import path from 'path'
 
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `Necesitas ingresar un nombre para continuar.`, m, rcanal)
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+  const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
+  const configPath = join('./Serbot', botActual, 'config.json')
+
+  let nombreBot = global.namebot || 'Anya Forger'
+
+  if (fs.existsSync(configPath)) {
+    try {
+      const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
+      if (config.name) nombreBot = config.name
+    } catch (err) { }
+  }
+  
+  if (!text) return conn.reply(m.chat, `🎀 Necesito un nombre para continuar, cielo.\n¿Podrías decírmelo con dulzura? 🌸\n\n𝟏 :: Ejemplo :: .setbotname BLACKPINK\n𝟐 :: Ejemplo :: .setbotname Gatitos\n𝟑 :: Ejemplo :: .setbotname LaLisa\n\n🍓 Asistente :: ${nombreBot}\n\n> LOVELLOUD Official`, m, rcanal)
 
   const senderNumber = m.sender.replace(/[^0-9]/g, '')
   const botPath = path.join('./Serbot', senderNumber)
   const configPath = path.join(botPath, 'config.json')
 
   if (!fs.existsSync(botPath)) {
-    return conn.reply(m.chat, 'No hay ninguna sesión activa vinculada a tu número.', m, rcanal)
+    return conn.reply(m.chat, `💭 Lo siento, no encontré ninguna sesión activa vinculada a tu número...\n\n🌸 Puede que aún no te hayas conectado\n🍥 O quizá tu sesión haya expirado sin avisarme\n🪷 Si deseas iniciar una nueva, estaré aquí para ayudarte\n\n🎀 Usa el comando :: .qr o .code para comenzar\n🍓 Asistente :: ${nombreBot}\n\n> LOVELLOUD Official`, m, rcanal)
   }
 
   let config = {}
@@ -24,7 +36,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 
   try {
     fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
-    return conn.reply(m.chat, `${text.trim()} será el nombre visible desde ahora.`, m, rcanal)
+    return conn.reply(m.chat, `🪷 ¡Nuevo nombre recibido con gracia!\n\n🎀 Ahora me llamaré :: ${text.trim()}\n🌸 Qué bonito suena... ¿verdad?\n🍥 Estoy list@ para servirte como siempre, pero con un toque renovado ✨\n\n💮 Si cambias de opinión, puedes volver a nombrarme cuando gustes.\n\n🍓 Asistente :: ${nombreBot}\n\n> LOVELLOUD Official`, m, rcanal)
   } catch {}
 }
 
