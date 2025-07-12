c5d24b278c9f34e2d0dd5c5bf384b393672ecc3f
LOVELLOUD <lovelloud@gmail.com>
2025-07-12 13:17:37 -0500

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,14 +1,27 @@
 import { execSync } from 'child_process'
-let handler = async (m, { conn, text }) => {
-await m.react('🕓')
-if (conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-await conn.reply(m.chat, stdout.toString(), m)
-await m.react('✅')
-}}
+
+let handler = async (m, { conn, text, isROwner }) => {
+  await m.react('🕓')
+
+  if (!isROwner) {
+    await conn.reply(m.chat, '✖️ Este comando solo puede ser utilizado por el *Creador* de la Bot.', m)
+    await m.react('❌')
+    return
+  }
+
+  try {
+    let stdout = execSync('git pull' + (text ? ' ' + text : ''))
+    await conn.reply(m.chat, stdout.toString(), m)
+    await m.react('✅')
+  } catch (err) {
+    await conn.reply(m.chat, `❌ Ocurrió un error:\n\n${err.toString()}`, m)
+    await m.react('❌')
+  }
+}
+
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.command = ['update', 'actualizar', 'fix', 'fixed']
 handler.rowner = true
 
 export default handler
