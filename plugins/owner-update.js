5a5d25d478a2f9d37e26a832b473710ceb1f4424
LOVELLOUD <lovelloud@gmail.com>
2025-07-15 14:22:50 -0500

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,14 +1,27 @@
 import { execSync } from 'child_process'
+
 let handler = async (m, { conn, text }) => {
-await m.react('🕓')
-if (conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-await conn.reply(m.chat, stdout.toString(), m)
-await m.react('✅')
-}}
+  m.react = async emoji => {
+    await conn.sendMessage(m.chat, {
+      react: {
+        text: emoji,
+        key: m.key
+      }
+    })
+  }
+
+  await m.react('🕓')
+
+  if (conn.user.jid == conn.user.jid) {
+    let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+    await conn.reply(m.chat, stdout.toString(), m)
+    await m.react('✅')
+  }
+}
+
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.command = ['update', 'actualizar', 'fix', 'fixed']
 handler.rowner = true
 
 export default handler
