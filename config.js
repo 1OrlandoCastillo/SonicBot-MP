c865ab25b9c69928c04a22c431cb2aee6fa38fcb
LOVELLOUD <lovelloud@gmail.com>
2025-07-15 13:55:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,29 +1,27 @@
-import { watchFile, unwatchFile } from 'fs' 
-import chalk from 'chalk'
+import { watchFile, unwatchFile } from 'fs'
 import { fileURLToPath } from 'url'
+import chalk from 'chalk'
 
 global.owner = [
   ['51942501966', 'Sung', true],
+  ['51928303585', 'Sung', true],
 ]
 
 global.sessions = 'Sessions'
-global.bot = 'Serbot' 
-global.AFBots = true
-
-global.packname = 'LOVELLOUD'
+global.bot = 'Serbot'
 global.namebot = 'Anya Forger'
+global.packname = 'LOVELLOUD'
 global.author = 'Sung'
 
 global.canal = 'https://whatsapp.com/channel/0029VbAZUQ3002T9KZfx2O1M'
-
 global.ch = {
-ch1: '120363403143798163@newsletter',
+  ch1: '120363403143798163@newsletter',
 }
 
+global.AFBots = true
 global.mods = []
 global.prems = []
-
-global.multiplier = 69 
+global.multiplier = 69
 global.maxwarn = '2'
 
 let file = fileURLToPath(import.meta.url)
