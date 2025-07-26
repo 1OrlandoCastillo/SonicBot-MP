630766050b3785964e7010b639da668ddcac1410
LOVELLOUD <lovelloud@gmail.com>
2025-07-26 13:21:39 -0500

    Delete plugins/owner-whoami.js

--- plugins/owner-whoami.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { isROwner, isOwner }) => {
-  m.reply(`📌 Tu JID: ${m.sender}
-✅ isROwner: ${isROwner}
-✅ isOwner: ${isOwner}`)
-}
-handler.command = /^whoami$/i
-
-export default handler
