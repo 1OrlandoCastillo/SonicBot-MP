862b3a5b17af0dc173068738649a6f0402d43faf
LOVELLOUD <lovelloud@gmail.com>
2025-07-27 15:33:36 -0500

    Update subbots-join.js

--- plugins/subbots-join.js
+++ plugins/subbots-join.js
@@ -1,14 +1,15 @@
-import path from 'path'
-
 let handler = async (m, { conn, command }) => {
   if (!m.isGroup) {
     return conn.reply(m.chat, `❖ El comando *${command}* solo puede ser usado en grupos.`, m, rcanal)
   }
 
-  const sender = m.sender.split('@')[0]
-  const botNumber = conn.user?.id?.split('@')[0]  // JID sin el "@s.whatsapp.net"
+  const botNumber = (conn.user?.id || conn.user?.jid || '').split('@')[0]
+  const senderNumber = m.sender.split('@')[0]
 
-  if (sender !== botNumber) {
+  console.log('BOT:', botNumber)
+  console.log('SENDER:', senderNumber)
+
+  if (senderNumber !== botNumber) {
     return conn.reply(m.chat, `❖ El comando *${command}* solo puede ser usado por el dueño del número del *sub-bot*.`, m, rcanal)
   }
 
