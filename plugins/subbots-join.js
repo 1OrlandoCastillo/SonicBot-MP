8ba5bcade2dac40f78f7bf573c8f108af6f1bf5b
LOVELLOUD <lovelloud@gmail.com>
2025-07-27 15:42:14 -0500

    Update subbots-join.js

--- plugins/subbots-join.js
+++ plugins/subbots-join.js
@@ -1,10 +1,20 @@
-let handler = async (m, { conn, command }) => {
-  if (!m.isGroup) {
-    return conn.reply(m.chat, `❖ El comando *${command}* solo puede ser usado en grupos.`, m, rcanal)
+import fs from 'fs'
+import path from 'path'
+import { join } from 'path'
+
+let handler = async (m, { conn, args, command }) => {
+  const botActual = conn.user?.id?.match(/\d{5,15}/)?.[0] || ''
+  const senderNumber = m.sender.match(/\d{5,15}/)?.[0] || ''
+
+  console.log('BOT:', conn.user?.id)
+  console.log('SENDER:', m.sender)
+
+  if (senderNumber !== botActual) {
+    return conn.reply(m.chat, `❖ El comando *${command}* solo puede ser usado por el dueño del número del *sub-bot*.`, m, rcanal)
   }
 
-  if (m.sender !== conn.user.jid) {
-    return conn.reply(m.chat, `❖ El comando *${command}* solo puede ser usado por el dueño del número del *sub-bot*.`, m, rcanal)
+  if (!m.isGroup) {
+    return conn.reply(m.chat, `❖ El comando *${command}* solo puede ser usado en grupos.`, m, rcanal)
   }
 
   await conn.reply(m.chat, `❖ Gracias por permitirme ser parte de este grupo.\n\nEstoy partiendo ahora.`, m, rcanal)
