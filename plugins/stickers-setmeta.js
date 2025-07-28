1ef5ee2bce762cdd4460d7dc27c61a276fdc5f4d
LOVELLOUD <lovelloud@gmail.com>
2025-07-28 13:43:28 -0500

    Update stickers-setmeta.js

--- plugins/stickers-setmeta.js
+++ plugins/stickers-setmeta.js
@@ -4,29 +4,21 @@ let handler = async (m, { text }) => {
   }
 
   let packname, author
-  let parts = text.split('|').map(v => v.trim())
+  let parts = text.split('|')
 
-  if (parts.length === 1) {
-    packname = parts[0]
-  } else {
-    [packname, author] = parts
-  }
+  packname = parts[0]?.trim()
+  author = parts[1]?.trim()
 
   if (!packname && !author) {
     return conn.reply(m.chat, `《✧》No se detectó ningún dato válido. Usa el formato:\n> *pack* | autor\n> Ejemplo: *Forger* | Stickers`, m, rcanal)
   }
 
   let user = global.db.data.users[m.sender]
-  if (packname) user.packname = packname
-  if (author) user.author = author
+
+  if (typeof packname === 'string') user.packname = packname || user.packname
+  if (typeof author === 'string') user.author = author || user.author
 
   return conn.reply(m.chat, `✐ Se actualizó el *pack* y/o *autor* por defecto para tus stickers.${
     packname ? `\n> Pack: *${user.packname}*` : ''
   }${author ? `\n> Autor: *${user.author}*` : ''}`, m, rcanal)
 }
-
-handler.help = ['setmeta <pack> | <autor>']
-handler.tags = ['stickers']
-handler.command = /^setmeta$/i
-
-export default handler
