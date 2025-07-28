a79546279c7adcfbce2767fc863efbf06428bfe8
LOVELLOUD <lovelloud@gmail.com>
2025-07-28 11:33:04 -0500

    Update perfiles-marry.js

--- plugins/perfiles-marry.js
+++ plugins/perfiles-marry.js
@@ -1,7 +1,10 @@
 let handler = async (m, { conn, mentionedJid }) => {
-  const pareja = mentionedJid[0]
+  const pareja = mentionedJid?.[0]
   if (!pareja)
     return conn.reply(m.chat, '《✧》Menciona a alguien para casarte. Ej: #marry @usuario', m, rcanal)
+
+  global.db.data.users[m.sender] = global.db.data.users[m.sender] || {}
+  global.db.data.users[pareja] = global.db.data.users[pareja] || {}
 
   global.db.data.users[m.sender].partner = pareja
   global.db.data.users[pareja].partner = m.sender
@@ -10,7 +13,7 @@ let handler = async (m, { conn, mentionedJid }) => {
 ✿ Matrimonio realizado
 
 ✦ Ahora estás casado con @${pareja.split('@')[0]}
-  `.trim(), m, { mentions: [pareja], rcanal })
+  `.trim(), m, rcanal, { mentions: [pareja] })
 }
 
 handler.help = ['#marry • #casarse + @usuario\n→ Cásate con otra persona y forma una pareja en tu perfil.']
