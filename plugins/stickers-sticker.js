d19d1db9fc0299232454c91a8b415837ecec1aab
LOVELLOUD <lovelloud@gmail.com>
2025-07-28 12:08:32 -0500

    Update stickers-sticker.js

--- plugins/stickers-sticker.js
+++ plugins/stickers-sticker.js
@@ -1,20 +1,23 @@
 let handler = async (m, { conn, quoted }) => {
-  if (!quoted || !/image|video/.test(quoted.mimetype))
-    return conn.reply(m.chat, '✦ Cita una imagen o video para convertir a sticker.', m)
+  let q = quoted || m.quoted
+
+  // Verifica si es una imagen o video
+  if (!q || !(q.mimetype || q.type) || !/image|video/.test(q.mimetype || q.type))
+    return conn.reply(m.chat, '✦ Cita una imagen o video para convertir en sticker.', m)
 
   conn.reply(m.chat, '✦ Generando tu sticker...', m)
 
   try {
-    let media = await quoted.download()
-    let sticker = await conn.sendImageAsSticker(m.chat, media, m, {
+    let media = await q.download()
+    await conn.sendImageAsSticker(m.chat, media, m, {
       packname: 'Bot Oficial',
       author: '★ StarCor'
     })
   } catch (e) {
-    conn.reply(m.chat, '✦ Ocurrió un error al crear el sticker.', m)
     console.error(e)
+    conn.reply(m.chat, '✦ Ocurrió un error al crear el sticker.', m)
   }
 }
 
-handler.command = /^sticker|s|stickers$/i
+handler.command = /^s|sticker|stickers$/i
 export default handler
