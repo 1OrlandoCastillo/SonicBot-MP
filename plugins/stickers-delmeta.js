347cfb889c01fbbbe763443aac63ddeb0d103e6c
LOVELLOUD <lovelloud@gmail.com>
2025-07-28 14:07:09 -0500

    Update stickers-delmeta.js

--- plugins/stickers-delmeta.js
+++ plugins/stickers-delmeta.js
@@ -7,7 +7,7 @@ let handler = async (m) => {
   return conn.reply(m.chat, `✐ Se restablecieron el *pack* y *autor* por defecto para tus stickers.`, m, rcanal)
 }
 
-handler.help = ['delstickermeta', 'delmeta']
+handler.help = ['#delstickermeta • #delmeta\n→ Restablece el pack y autor por defecto de tus stickers']
 handler.tags = ['stickers']
 handler.command = /^delstickermeta|delmeta$/i
 
