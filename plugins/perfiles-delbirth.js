d48c116837d7de0fbc3dbd11b5393358193b575d
LOVELLOUD <lovelloud@gmail.com>
2025-07-28 10:01:04 -0500

    Add files via upload

--- plugins/perfiles-delbirth.js
+++ plugins/perfiles-delbirth.js
@@ -1,8 +1,10 @@
 let handler = async (m) => {
-  if (!global.db.data.users[m.sender].birth) throw '《✧》No tienes un cumpleaños registrado por el momento.'
+  if (!global.db.data.users[m.sender].birth)
+    return conn.reply(m.chat, '《✧》No tienes un cumpleaños registrado por el momento.', m, rcanal)
+    
   delete global.db.data.users[m.sender].birth
-  return conn.reply(m.chat, `
-「✐」Se ha eliminado tu cumpleaños correctamente.`, m, rcanal)
+  return conn.reply(m.chat, '「✐」Se ha eliminado tu cumpleaños correctamente.', m, rcanal)
 }
+
 handler.command = /^delbirth$/i
 export default handler
