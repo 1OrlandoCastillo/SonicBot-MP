1cd57584f1962aa0c9acce9766c30964bfdb1d38
LOVELLOUD <lovelloud@gmail.com>
2025-07-28 14:03:11 -0500

    Add files via upload

---
+++ plugins/stickers-delmeta.js
@@ -0,0 +1,14 @@
+let handler = async (m) => {
+  let user = global.db.data.users[m.sender]
+
+  user.packname = ''
+  user.author = ''
+
+  return conn.reply(m.chat, `✐ Se restablecieron el *pack* y *autor* por defecto para tus stickers.`, rcanal)
+}
+
+handler.help = ['delstickermeta', 'delmeta']
+handler.tags = ['stickers']
+handler.command = /^delstickermeta|delmeta$/i
+
+export default handler
