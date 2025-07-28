dda9870ee28c4306cef0b661bd0120457fc78bba
LOVELLOUD <lovelloud@gmail.com>
2025-07-28 11:15:16 -0500

    Update perfiles-setdesc.js

--- plugins/perfiles-setdesc.js
+++ plugins/perfiles-setdesc.js
@@ -6,7 +6,7 @@ let handler = async (m, { text}) => {
 
   return conn.reply(m.chat, `✐ Tu descripción ha sido guardada como *${text}*!`, m, rcanal)
 }
-handler.help = ['#setdescription • #setdesc* + [Descripción]\n→ Establece una descripción única para tu perfil']
+handler.help = ['#setdescription • #setdesc + [Descripción]\n→ Establece una descripción única para tu perfil']
 handler.tags = ['perfiles']
 handler.command = /^setdesc|setdescription$/i
 export default handler
