f5fc03be9ea648886832623cce37b539f21401bb
Orlando <orlandocastillomaria26@gmail.com>
2025-09-04 10:45:17 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,42 +1,40 @@
-import { watchFile, unwatchFile } from 'fs' 
+import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
+// 👑 Dueños
 global.owner = [
-  ['51942501966', 'Sung', true],
-  ['51901437507', 'Sunkovv', true],
+  ['5212731590195', 'White', true],
+  ['12543120750', 'adri', true],
 ]
-
 
 global.ownerLid = [
-  ['114263544885392', 'Sunkovv', true],
-  ['78186205446362', 'Sung', true],
-  ['38148520288338', 'Sung2', true],
-
-
+  ['5212731590195', 'White', true],
+  ['5212731590195', 'White2', true],
 ]
 
+// ⚙️ Configuración del bot
 global.sessions = 'Sessions'
-global.bot = 'Serbot' 
+global.bot = 'Serbot'
 global.AFBots = true
 
 global.packname = 'LOVELLOUD'
 global.namebot = 'KIYOMI MD'
 global.author = 'Sung'
 
-
+// 📢 Canales y newsletters
 global.canal = 'https://whatsapp.com/channel/0029VbAZUQ3002T9KZfx2O1M'
-
 global.ch = {
-ch1: '120363403143798163@newsletter',
+  ch1: '120363403143798163@newsletter',
 }
 
+// 🔧 Otros
 global.mods = []
 global.prems = []
+global.multiplier = 69
+global.maxwarn = 2
 
-global.multiplier = 69 
-global.maxwarn = '2'
-
+// 📌 Auto-reload del archivo
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
